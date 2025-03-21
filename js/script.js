d83aa9d334c22fb31217f91d7513831b1bdd9972
T-Naing <thitnaing86@gmail.com>
2025-03-21 14:42:10 +0630

    replace bootstrap 3 with 5

---
+++ js/script.js
@@ -0,0 +1,87 @@
+var aboutUrl = "./snippets/about_snippet.html";
+var homeUrl = "./snippets/home_snippet.html";
+
+var insertHtml = function(selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+var showLoading = function(selector) {
+  var html = '<div class="d-flex justify-content-center">';
+  html += '<div class="spinner-border" role="status">';
+  html += '<span class="visually-hidden" >Loading...</span></div></div>';
+
+  insertHtml(selector, html);
+};
+
+var loadAbout=function (){
+  // console.log("About loading..")
+  showLoading("#main-content");
+    var requestObj = new XMLHttpRequest();
+    requestObj.onreadystatechange = function() {
+      if (requestObj.readyState === 4 && requestObj.status === 200) {
+        document.querySelector("#main-content")
+          .innerHTML = requestObj.responseText;
+        switchMenuToActive();
+      }
+    };
+    requestObj.open("Get", aboutUrl, true);
+    requestObj.send();    
+}
+
+var switchMenuToActive=function () {
+  // console.log("active");
+  var classes=document.querySelector("#navHomeButton").className;
+  classes=classes.replace(new RegExp("active","g"),"");
+  document.querySelector("#navHomeButton").className=classes;
+
+  classes=document.querySelector("#navAboutButton").className;
+  if(classes.indexOf("active")==-1){
+    classes+=" active";
+    classes=document.querySelector("#navAboutButton").className=classes;
+  }
+}
+
+document.addEventListener("DOMContentLoaded",
+  function(event) {
+
+    showLoading("#main-content");
+    var requestObj = new XMLHttpRequest();
+    requestObj.onreadystatechange = function() {
+      if (requestObj.readyState === 4 && requestObj.status === 200) {
+        document.querySelector("#main-content")
+          .innerHTML = requestObj.responseText;
+      }
+    };
+    requestObj.open("Get", homeUrl, true);
+    requestObj.send();
+
+    const navbarToggle = document.getElementById("navbarToggle");
+    const collapsableNav = document.getElementById("navbarNav");
+
+    navbarToggle.addEventListener("blur", function() {
+      const screenWidth = window.innerWidth;
+      // if (screenWidth < 768){
+      if(screenWidth<992) {
+        collapsableNav.classList.remove("show"); // Hide the collapsible navigation
+      }
+    });
+
+    navbarToggle.addEventListener("click", function(event) {
+      event.target.focus();
+    });
+        
+    const button = document.getElementById('languageDropdown');
+    const defaultItem = document.querySelector('.dropdown-item'); // Select the first dropdown item
+    button.textContent = defaultItem.textContent; // Set the button text to the first item's text
+
+    // Dropdown item click listener
+    document.querySelectorAll('.dropdown-item').forEach(item => {
+      item.addEventListener('click', function() {
+        console.log("dropdown-item");
+        const button = document.getElementById('languageDropdown');
+        button.textContent = this.textContent; // Update the button text based on the selected item
+      });
+    });
+
+  });
